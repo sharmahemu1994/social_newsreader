16eb851ab3b07d5d7c7a919ea31057ddd6c94eba
Himanshu <himanshu@Admins-MacBook-Pro-2.local>
2019-07-01 01:37:14 +0530

    assignment done

---
+++ getEntity.js
@@ -0,0 +1,31 @@
+
+const getEntities = () => {
+	return (
+		[
+			{
+				start: 14,
+				end: 22,
+				value: 'entity'
+			},
+			{
+				start: 0,
+				end: 5,
+				value: 'entity'
+			},
+			{
+				start: 55,
+				end: 67,
+				value: 'twitter_username'
+			},
+			{
+				start: 37,
+				end: 54,
+				value: 'link'
+			}
+		]
+	)
+};
+
+module.exports = {
+	getEntities
+};
---
+++ getText.js
@@ -0,0 +1,8 @@
+
+const getText = () => {
+	return ('Obama visited Facebook headquarters: http://bit.ly/xyz @elversatile');
+};
+
+module.exports = {
+	getText
+};
